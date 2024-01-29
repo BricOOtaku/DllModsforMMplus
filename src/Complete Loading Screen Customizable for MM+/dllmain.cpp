ba646d4d37f29924402131b12a4f3c6d234d608d
BricOOtaku <90196012+BricOOtaku@users.noreply.github.com>
2024-01-29 15:10:06 +0100

    [CLSC 2.0] - Code Cleanup + others tweaks
    Alternate text for Mix Mode (MM/PS4)
    Increased Customizable images up to 1019

--- src/Complete Loading Screen Customizable for MM+/dllmain.cpp
+++ src/Complete Loading Screen Customizable for MM+/dllmain.cpp
@@ -9,6 +9,7 @@
 int rnd;
 int rnd2;
 uint8_t rorn = 'r';
+uint8_t txorpf[] = { 'p', 'f' };
 
 int randomLoading = 1;
 int loadingStyle = -1;
@@ -18,19 +19,19 @@ SIG_SCAN(sigLoadingBg, 0x140CC2158, "\x6C\x6F\x61\x64\x69\x6E\x67\x5F\x62\x67", 
 SIG_SCAN(sigNowLoading, 0x140CC2128, "\x6E\x6F\x77\x5F\x6C\x6F\x61\x64\x69\x6E\x67", "xxxxxxxxxxx")
 SIG_SCAN(sigLoadingScreen, 0x140654280, "\x48\x89\x5C\x24\x00\x48\x89\x7C\x24\x00\x55\x48\x8D\xAC\x24\x00\x00\x00\x00\x48\x81\xEC\x00\x00\x00\x00\x48\x8B\x05\x00\x00\x00\x00\x48\x33\xC4\x48\x89\x85\x00\x00\x00\x00\x48\x8B\xF9\x45\x33\xC0\x41\x8D\x50\x04\x33\xC9\xE8\x00\x00\x00\x00\x8B\xD8\x48\x8D\x15\x00\x00\x00\x00", "xxxx?xxxx?xxxxx????xxx????xxx????xxxxxx????xxxxxxxxxxxxx????xxxxx????")
 
-SIG_SCAN(sigRightsList, 0x140CC21E8, "\x72\x69\x67\x68\x74\x73\x5F\x6C\x69\x73\x74", "xxxxxxxxxxx")
-SIG_SCAN(sigRightsBg01, 0x140CC2178, "\x72\x69\x67\x68\x74\x73\x5F\x62\x67\x30\x31", "xxxxxxxxxx?x")
-SIG_SCAN(sigRightsBg02, 0x140CC2168, "\x72\x69\x67\x68\x74\x73\x5F\x62\x67\x30\x32", "xxxxxxxxxx?x")
+SIG_SCAN(sigRightsList, 0x140CC21E8, "rights_list", "xxxxxxxxxxx")
+SIG_SCAN(sigRightsBg01, 0x140CC2178, "rights_bg01", "xxxxxxxxxx?x")
+SIG_SCAN(sigRightsBg02, 0x140CC2168, "rights_bg02", "xxxxxxxxxx?x")
 
-SIG_SCAN(sigRightsBaseMan, 0x140CC2208, "\x72\x69\x67\x68\x74\x73\x5F\x62\x61\x73\x65\x5F\x6D\x61\x6E", "xxxxxxxxxxxxxxx")
-SIG_SCAN(sigRightsBaseArr, 0x140CC2218, "\x72\x69\x67\x68\x74\x73\x5F\x62\x61\x73\x65\x5F\x61\x72\x72", "xxxxxxxxxxxxxxx")
-SIG_SCAN(sigRightsBaseLyr, 0x140CC2228, "\x72\x69\x67\x68\x74\x73\x5F\x62\x61\x73\x65\x5F\x6C\x79\x72", "xxxxxxxxxxxxxxx")
-SIG_SCAN(sigRightsBaseMus, 0x140CC21A8, "\x72\x69\x67\x68\x74\x73\x5F\x62\x61\x73\x65\x5F\x6D\x75\x73", "xxxxxxxxxxxxxxx")
-SIG_SCAN(sigRightsBasePv, 0x140CC2250, "\x72\x69\x67\x68\x74\x73\x5F\x62\x61\x73\x65\x5F\x70\x76", "xxxxxxxxxxxxxx")
-SIG_SCAN(sigRightsBaseGui, 0x140CC21F8, "\x72\x69\x67\x68\x74\x73\x5F\x62\x61\x73\x65\x5F\x67\x75\x69", "xxxxxxxxxxxxxxx")
+SIG_SCAN(sigRightsBaseMan, 0x140CC2208, "rights_base_man", "xxxxxxxxxxxxxxx")
+SIG_SCAN(sigRightsBaseArr, 0x140CC2218, "rights_base_arr", "xxxxxxxxxxxxxxx")
+SIG_SCAN(sigRightsBaseLyr, 0x140CC2228, "rights_base_lyr", "xxxxxxxxxxxxxxx")
+SIG_SCAN(sigRightsBaseMus, 0x140CC21A8, "rights_base_mus", "xxxxxxxxxxxxxxx")
+SIG_SCAN(sigRightsBasePv, 0x140CC2250, "rights_base_pv", "xxxxxxxxxxxxxx")
+SIG_SCAN(sigRightsBaseGui, 0x140CC21F8, "rights_base_gui", "xxxxxxxxxxxxxxx")
 
-SIG_SCAN(sigPRightsName02dLt, 0x140CC2238, "\x70\x5F\x72\x69\x67\x68\x74\x73\x5F\x6E\x61\x6D\x65\x25\x30\x32\x64\x5F\x6C\x74", "xxxxxxxxxxxxxx?xxxxx")
-SIG_SCAN(sigPRightsSongLt, 0x140CC21D0, "\x70\x5F\x72\x69\x67\x68\x74\x73\x5F\x73\x6F\x6E\x67\x5F\x6C\x74", "xxxxxxxxxxxxxxxx")
+SIG_SCAN(sigPRightsName02dLt, 0x140CC2238, "p_rights_name%02d_lt", "xxxxxxxxxxxxxxxxxxxx")
+SIG_SCAN(sigPRightsSongLt, 0x140CC21D0, "p_rights_song_lt", "xxxxxxxxxxxxxxxx")
 
 void load_bg(unsigned char v1, unsigned char v2, unsigned char v3, unsigned char v4) {
 
@@ -64,58 +65,71 @@ void random_bg() {
 
 void set_load_style(int x1) {
 
+	uint8_t loadStyle[3];
 	switch (x1) {
 
 	case 1: //X
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'p', 'j', 'x');
+		loadStyle[0] = 'p', loadStyle[1] = 'j', loadStyle[2] = 'x';
 		break;
 
 	case 2: //FT
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'p', 'f', 't');
+		loadStyle[0] = 'p', loadStyle[1] = 'f', loadStyle[2] = 't';
 		break;
 
 	case 3: //F2nd
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'd', 'f', '2');
+		loadStyle[0] = 'd', loadStyle[1] = 'f', loadStyle[2] = '2';
 		break;
 
 	case 4: //F
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'p', 'j', 'f');
+		loadStyle[0] = 'p', loadStyle[1] = 'j', loadStyle[2] = 'f';
 		break;
 
 	case 5: //DTEX and DT2nd (PS3)
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'p', 's', '3');
+		loadStyle[0] = 'p', loadStyle[1] = 's', loadStyle[2] = '3';
 		break;
 
 	case 6: //EX and 2nd (PSP)
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'p', 's', 'p');
+		loadStyle[0] = 'p', loadStyle[1] = 's', loadStyle[2] = 'p';
 		break;
 
 	case 7: //1st
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'p', 'j', 'd');
+		loadStyle[0] = 'p', loadStyle[1] = 'j', loadStyle[2] = 'd';
 		break;
 
 	default: //M39's
+		loadStyle[0] = 'n', loadStyle[1] = 'o', loadStyle[2] = 'w';
 		rorn = 'r';
-		WRITE_MEMORY(sigNowLoading(), uint8_t, 'n', 'o', 'w');
+		txorpf[0] = 't', txorpf[1] = 'x';
 		break;
 	}
 
-	if (x1 != 0) {
+	WRITE_MEMORY(sigNowLoading(), uint8_t, loadStyle[0], loadStyle[1], loadStyle[2]);
 
+	if (x1 != 0) {
 		rorn = 'n';
+		txorpf[0] = 'p', txorpf[1] = 'f';
 	}
 
-	WRITE_MEMORY(sigRightsList(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBg01(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBg02(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBaseMan(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBaseArr(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBaseLyr(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBaseMus(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBasePv(), uint8_t, rorn);
-	WRITE_MEMORY(sigRightsBaseGui(), uint8_t, rorn);
-	WRITE_MEMORY((char*)sigPRightsName02dLt() + 0x02, uint8_t, rorn);
-	WRITE_MEMORY((char*)sigPRightsSongLt() + 0x02, uint8_t, rorn);
+	WRITE_MEMORY(0x140CC2188, uint8_t, txorpf[0], txorpf[1]);
+	WRITE_MEMORY(0x140CC2198, uint8_t, txorpf[0], txorpf[1]);
+
+	void* RornArrays[] = {
+		sigRightsList(),
+		sigRightsBg01(),
+		sigRightsBg02(),
+		sigRightsBaseMan(),
+		sigRightsBaseArr(),
+		sigRightsBaseLyr(),
+		sigRightsBaseMus(),
+		sigRightsBasePv(),
+		sigRightsBaseGui(),
+		(char*)sigPRightsName02dLt() + 0x02,
+		(char*)sigPRightsSongLt() + 0x02,
+	};
+	
+	for (int i = 0; i < sizeof(RornArrays) / sizeof(RornArrays[0]); i++) {
+		WRITE_MEMORY(RornArrays[i], uint8_t, rorn);
+	}
 }
 
 void random_load() {
@@ -165,9 +179,9 @@ extern "C" __declspec(dllexport) void Init() {
 
 		randomLoading = 1;
 	}
-	else if (randomLoading > 1003) {
+	else if (randomLoading > 1019) {
 
-		randomLoading = 1003;
+		randomLoading = 1019;
 	}
 
 	srand(time(NULL));
